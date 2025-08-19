01d22719f5f3608e1d843a4e81163ffd1acb7c92
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-08-19 20:04:15 +0530

    add logging in PersonServlet

--- src/main/java/servlets/PersonServlet.java
+++ src/main/java/servlets/PersonServlet.java
@@ -2,6 +2,9 @@ package servlets;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import enterprise.Person;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -10,6 +13,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class PersonServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7274317475921056871L;
+
+	public static final Logger log = LoggerFactory.getLogger(PersonServlet.class);
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String fname = req.getParameter("fname");
@@ -20,7 +30,7 @@ public class PersonServlet extends HttpServlet{
 		String fullName = fname + " " + lname;
 		
 		
-		System.out.println("Welcome " + fullName);
+		log.info("Welcome " + fullName);
 		
 		req.setAttribute("name", fullName);
 		RequestDispatcher dispatcher = req.getRequestDispatcher("welcome.jsp");
